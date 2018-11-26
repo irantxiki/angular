a9385a007ca5edde4f8b0032e3a5a02a41ed002d
Irantzu Marquinez <imarquinez@qualityobjects.com>
2018-11-26 11:39:55 +0100

    Commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Link } from './link/link.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  links: Link[];
+
+  constructor() {
+    this.links = [
+      new Link('angular', 'http://angular.io', 10),
+      new Link('google', 'http://google.com', 100),
+      new Link('youtube', 'http://youtube.com', 1000)
+    ];
+  }
+
+  addLink(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log('Titulo: ' + title.value);
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedLinks() {
+    return this.links.sort((a: Link, b: Link) => b.votes - a.votes);
+  }
+
+}
