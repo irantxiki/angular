80ee35d11ade5d97520828ba1796cfaa40e00766
alberjt <ajimenez@qualityobjects.com>
2019-01-03 14:01:07 +0100

    CMC-724

--- src/app/modelo/votaciones.model.ts
+++ src/app/modelo/votaciones.model.ts
@@ -1,21 +1,16 @@
+import { Observable } from 'rxjs';
+
 export class Votaciones {
     id: number;
     titulo: string;
     enlace: string;
     numero: number;
+    animacion: boolean;
 
     constructor(title: string, link: string, votes?: number, id?: number) {
         this.id = id;
         this.titulo = title;
         this.enlace = link;
         this.numero = votes || 0;
-    }
-
-    votoPositivo(): void {
-        this.numero++;
-    }
-
-    votoNegativo(): void {
-        this.numero--;
     }
 }
