e667f60b3f0c40065c07a3481d8b7230574a665a
Irantzu Marquinez <imarquinez@qualityobjects.com>
2019-01-04 15:33:49 +0100

    label

--- src/app/componentes/nueva-votacion/nueva-votacion.component.ts
+++ src/app/componentes/nueva-votacion/nueva-votacion.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Votaciones } from '../../modelo/votaciones.model';
 import { VotacionesService } from '../../servicios/votaciones.service';
 import { ElasticsearchService } from 'src/app/servicios/elasticsearch.service';
