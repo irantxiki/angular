bf58e39a1efdf4914ac8652959773cdc206f45aa
rredondog <45395862+rredondog@users.noreply.github.com>
2019-01-08 10:58:56 +0100

    CMC-699 NG: Ver detalle de votación para edición

--- src/app/componentes/nueva-votacion/nueva-votacion.component.ts
+++ src/app/componentes/nueva-votacion/nueva-votacion.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Votaciones } from '../../modelo/votaciones.model';
 import { VotacionesService } from '../../servicios/votaciones.service';
@@ -11,25 +11,58 @@ import { ElasticsearchService } from 'src/app/servicios/elasticsearch.service';
   templateUrl: './nueva-votacion.component.html',
   styleUrls: ['./nueva-votacion.component.css']
 })
-export class NuevaVotacionComponent implements OnInit {
+export class NuevaVotacionComponent implements OnInit, OnDestroy {
 
   loading: boolean;
   nuevaVotacionForm: FormGroup;
   submitted = false;
+  idEdit: any;
+  tituloVentana: string;
+  votacion: Votaciones;
+  private sub: any;
 
-  constructor(private formBuilder: FormBuilder, private router: Router, 
+  constructor(private formBuilder: FormBuilder, private router: Router,
+              private route: ActivatedRoute,
               private votacionesService: VotacionesService,
               private es: ElasticsearchService) { }
 
   ngOnInit(): void {
-    this.nuevaVotacionForm = this.formBuilder.group({
-      titulo: ['', [
-        Validators.required,
-        Validators.minLength(4)//,
-        //forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
-      ]],
-      enlace: ['']
+
+    this.sub = this.route.params.subscribe(params => {
+      console.log(params);
+      this.idEdit = params['id'];
+      console.log('RR 1');
+      console.log(this.idEdit);
+      console.log('RR 2');
     });
+
+    if (this.idEdit) {
+      this.tituloVentana = 'VOTACIONES.EDIT_VOTACION';
+
+      this.votacionesService.getVotacion(this.idEdit)
+        .subscribe(votacion => {
+          this.votacion = votacion;
+          console.log('Votacion Recuperada ' + this.votacion.titulo);
+        });
+
+      this.nuevaVotacionForm = this.formBuilder.group({
+        titulo: ['', [
+          Validators.required,
+          Validators.minLength(4)
+        ]],
+        enlace: ['']
+      });
+    } else {
+      this.tituloVentana = 'VOTACIONES.ADD_VOTACION';
+      this.nuevaVotacionForm = this.formBuilder.group({
+        titulo: ['', [
+          Validators.required,
+          Validators.minLength(4)// ,
+          // forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
+        ]],
+        enlace: ['']
+      });
+    }
   }
 
   // convenience getter for easy access to form fields
@@ -48,25 +81,25 @@ export class NuevaVotacionComponent implements OnInit {
   }
 
   addVotacion(titulo: string, enlace: string): boolean {
-    //console.log('Titulo: ' + titulo);
-    //console.log('Titulo: ' + enlace);
+    // console.log('Titulo: ' + titulo);
+    // console.log('Titulo: ' + enlace);
     var votacion = new Votaciones(titulo, enlace);
-    //this.votaciones.push(votacion);
+    // this.votaciones.push(votacion);
     this.saveVotacion(votacion);
     return false;
   }
 
   saveVotacion(votacion: Votaciones): void {
     this.loading = true;
-    
-    //Guardamos en Postgre
+
+    // Guardamos en Postgre
     this.votacionesService.crearVotacion(votacion)
         .subscribe( data => {
           this.loading = false;
-          //alert("User created successfully.");
+          // alert("User created successfully.");
         });
-        
-    //Guardamos en Elasticsearch
+
+    // Guardamos en Elasticsearch
     this.es.addToIndex({
       index: 'gkz_index',
       type: 'votacion',
@@ -87,5 +120,7 @@ export class NuevaVotacionComponent implements OnInit {
 
   }
 
-
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
